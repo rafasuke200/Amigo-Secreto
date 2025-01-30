81e1fc98dc7637a55804abe171b6bc5e4dc3f7bb
Rafael Mota <rafasuke200@gmail.com>
2025-01-30 14:48:06 -0300

    adicionada função para implementar amigos no array

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+
+
+function adicionarAmigo() {
+    let novoAmigo = document.getElementById('amigo').value;
+    amigos.push(novoAmigo);
+    console.log(amigos);
+}
